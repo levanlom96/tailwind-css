4effc1a23cf6c8a343bb6dda361d571d3d69a487
Levan <levanlom96@gmail.com>
2025-08-20 23:23:31 +0400

    learned to add fonts to the tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./dist/**/*.html", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        allura: ['Allura', 'cursive'],
+        'noto-serif': ['Noto Serif', 'serif'],
+      },
+    },
   },
   plugins: [],
 }
